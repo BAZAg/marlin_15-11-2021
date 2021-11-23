0e51947fa4a27f923940d29e3284df2469a0a468
Юрий Йосифович <aramic2007@gmail.com>
2021-11-23 10:22:19 +0200

    Update index.php
    Добавил ссылки на задание 15_2 по загрузке нескольких файлов

--- index.php
+++ index.php
@@ -101,6 +101,12 @@
             ["href"=> "task_15_1.html", "text" => "Задача"],
             ["href"=> "solution_task_15_1.php", "text"=>"Решение"],
             ["href"=> "db_task_15_1.json", "text"=>"json"]
+        ] ,
+        [
+            ["href"=> "description_task_15_2.php", "text"=>"Описание"],
+            ["href"=> "task_15_2.html", "text" => "Задача"],
+            ["href"=> "solution_task_15_2.php", "text"=>"Решение"],
+            ["href"=> "db_task_15_2.json", "text"=>"json"]
         ]
 
 
